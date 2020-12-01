900f48dca1b46a55e569705bda649e1b99ca32d6
ramang22 <wibrach@gmail.com>
2020-12-01 14:22:00 +0100

    working testing version

---
+++ data_gathering/data_gathering.ino
@@ -0,0 +1,153 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+//#include <U8g2lib.h>
+#include<Wire.h>
+#include <SD.h>
+
+
+// GPS PORTS
+#define TX 3
+#define RX 2
+// PIN setup for display
+#define EN 10
+#define RW 9
+#define RS 8
+
+
+// gyro act setup
+float AccX, AccY, AccZ;
+float GyroX, GyroY, GyroZ;
+float AccErrorX, AccErrorY, AccErrorZ, GyroErrorX, GyroErrorY, GyroErrorZ;
+int c = 0;
+
+const int MPU = 0x68;
+
+
+File myFile;
+
+SoftwareSerial swSerial(RX, TX);
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+  }
+  if (!SD.begin(4)) {
+    while (1);
+  }
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+  swSerial.begin(9600);
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);
+  Wire.write(0x10);
+  Wire.endTransmission(true);
+
+  calculate_IMU_error();
+}
+
+void loop() {
+  TinyGPS gps;
+  long gForce = 0;
+  int speed = 0;
+  char datumCas[32];
+  bool newDataLoaded = false;
+
+  File myFile = SD.open("khanke.txt", FILE_WRITE);
+
+  for (unsigned long start = millis(); millis() - start < 1000;) {
+    while (swSerial.available()) {
+      char c = swSerial.read();
+      if (gps.encode(c)) {
+        newDataLoaded = true;
+      }
+    }
+  }
+  if (newDataLoaded) {
+    unsigned long gpsData;
+    int year;
+    byte month, day, hour, minute, second, miliSecond;
+    speed = (gps.f_speed_kmph() == TinyGPS::GPS_INVALID_F_SPEED ? 0 : gps.f_speed_kmph());
+    gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &miliSecond, &gpsData);
+    sprintf(datumCas, "%02d/%02d/%02d %02d:%02d:%02d", month, day, year, hour, minute, second);
+    Wire.beginTransmission(MPU);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 12, true);
+    AccX = (Wire.read() << 8 | Wire.read()) / 2048.0;
+    AccX -= AccErrorX;
+    AccY = (Wire.read() << 8 | Wire.read()) / 2048.0;
+    AccY -= AccErrorY;
+    AccZ = (Wire.read() << 8 | Wire.read()) / 2048.0;
+    AccZ -= AccErrorZ;
+    GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
+    GyroX -= GyroErrorX;
+    GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
+    GyroX -= GyroErrorY;
+    GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+    GyroZ -= GyroErrorZ;
+
+    gForce = sqrt(AccX * AccX + AccY * AccY + AccZ * AccZ);
+    if (myFile) {
+      myFile.print(datumCas);
+      myFile.print(F(";"));
+      myFile.print(gForce);
+      myFile.print(F(";"));
+      myFile.print(speed);
+      myFile.print(F(";"));
+      myFile.print(AccX);
+      myFile.print(F(";"));
+      myFile.print(AccY);
+      myFile.print(F(";"));
+      myFile.print(AccZ);
+      myFile.print(F(";"));
+      myFile.print(GyroX);
+      myFile.print(F(";"));
+      myFile.print(GyroY);
+      myFile.print(F(";"));
+      myFile.print(GyroZ);
+      myFile.println(F(";"));
+      myFile.close();
+    }
+  }
+  delay(300);
+}
+
+void calculate_IMU_error() {
+  while (c < 200) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
+    // Sum all readings
+    AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
+    AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
+    c++;
+  }
+  AccErrorX = AccErrorX / 200;
+  AccErrorY = AccErrorY / 200;
+  c = 0;
+  while (c < 200) {
+    Wire.beginTransmission(MPU);
+    Wire.write(0x43);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU, 6, true);
+    GyroX = Wire.read() << 8 | Wire.read();
+    GyroY = Wire.read() << 8 | Wire.read();
+    GyroZ = Wire.read() << 8 | Wire.read();
+    // Sum all readings
+    GyroErrorX = GyroErrorX + (GyroX / 131.0);
+    GyroErrorY = GyroErrorY + (GyroY / 131.0);
+    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
+    c++;
+  }
+  GyroErrorX = GyroErrorX / 200;
+  GyroErrorY = GyroErrorY / 200;
+  GyroErrorZ = GyroErrorZ / 200;
+}
---
+++ drive/drive.ino
@@ -0,0 +1,126 @@
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+#include <U8g2lib.h>
+#include<Wire.h>
+
+
+// GPS PORTS
+#define TX 3
+#define RX 2
+// PIN setup for display
+#define EN 10
+#define RW 9
+#define RS 8
+//gyroo and acc errors
+#define ERRX 0
+#define ERRY 0
+#define ERRZ 0
+#define GERRX -1.57
+#define GERRY -1.07
+#define GERRZ 0.53
+
+// init display
+U8G2_ST7920_128X64_1_SW_SPI lcd(U8G2_R0, EN, RW, RS);
+int refresh = 0;
+
+// gyro act setup
+const int MPU = 0x68;
+int16_t  AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+SoftwareSerial swSerial(RX, TX);
+TinyGPS gps;
+
+void setup() {
+  Serial.begin(9600);
+
+  lcd.begin();
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Wire.beginTransmission(MPU);
+  Wire.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
+  Wire.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
+  Wire.endTransmission(true);
+
+  swSerial.begin(9600);
+}
+
+void loop() {
+  long gForce = 0;
+  int speed = 0;
+  char datumCas[32];
+  bool newDataLoaded = false;
+
+  for (unsigned long start = millis(); millis() - start < 1000;) {
+    while (swSerial.available()) {
+      char c = swSerial.read();
+      if (gps.encode(c)) {
+        newDataLoaded = true;
+      }
+    }
+  }
+  if (newDataLoaded) {
+    unsigned long gpsData;
+    int year;
+    byte month, day, hour, minute, second, miliSecond;
+    speed = (gps.f_speed_kmph() == TinyGPS::GPS_INVALID_F_SPEED ? 0 : gps.f_speed_kmph());
+    gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &miliSecond, &gpsData);
+    sprintf(datumCas, "%02d/%02d/%02d %02d:%02d:%02d", month, day, year, hour, minute, second);
+  }
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 12, true);
+  AcX = (Wire.read() << 8 | Wire.read()) / 2048.0;
+  AcX -= ERRX;
+  AcY = (Wire.read() << 8 | Wire.read()) / 2048.0;
+  AcY -= ERRY;
+  AcZ = (Wire.read() << 8 | Wire.read()) / 2048.0;
+  AcZ -= ERRZ;
+  GyX = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyX -= GERRX;
+  GyY = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyY -= GERRY;
+  GyZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyZ -= GERRZ;
+
+  gForce = sqrt(AcX * AcX + AcY * AcY + AcZ * AcZ);
+  Serial.println(AcX);
+  Serial.println(AcY);
+  Serial.println(AcZ);
+  if (millis() - refresh > 100) {
+    lcd.firstPage();
+    do {
+      printTest(gForce, speed);
+      //drawBitMap();
+    } while ( lcd.nextPage() );
+    refresh = millis();
+  }
+}
+
+
+void printTest(unsigned long gForce, int speed) {
+  lcd.setFont(u8g2_font_6x12_mf );
+  lcd.setCursor(0, 7);
+  lcd.print ("Speed : ");
+  lcd.print(speed);
+  lcd.print(" km/h");
+  lcd.setCursor(0, 21);
+  lcd.print("G-Force :");
+  lcd.print(gForce);
+  lcd.setCursor(0, 32);
+  lcd.print("GX :");
+  lcd.print(GyX);
+  lcd.setCursor(0, 42);
+  lcd.print("GY :");
+  lcd.print(GyY);
+  lcd.setCursor(0, 52);
+  lcd.print("GZ :");
+  lcd.print(GyZ);
+  lcd.setCursor(0, 63);
+  lcd.print("Satellites :");
+  lcd.print(gps.satellites());
+}
